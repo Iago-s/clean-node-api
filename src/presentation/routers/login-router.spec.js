4618ecd05c2da5e5995042c5a2ba3188a4f604b3
iago-s <iagooliveira3845@hotmail.com>
2021-07-08 15:11:24 -0300

    ensure login route calls AuthUseCase with correct params

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -4,12 +4,28 @@ const MissingParamError = require('../helpers/missing-param-error')
 // Evitar quando mudar a instancia de uma classe quebre todos
 // os testes
 const makeSut = () => {
-  return new LoginRouter()
+  // Classe mocada
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+
+  const authUseCaseSpy = new AuthUseCaseSpy()
+
+  // Injeção de dependencia
+  const sut = new LoginRouter(authUseCaseSpy)
+
+  return {
+    sut,
+    authUseCaseSpy
+  }
 }
 
 describe('login router', () => {
   test('Should return 400 if no email is provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -22,7 +38,7 @@ describe('login router', () => {
   })
 
   test('Should return 400 if no password is provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'iago@email.com'
@@ -35,17 +51,31 @@ describe('login router', () => {
   })
 
   test('Should return 500 if no httpRequest is provider', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.route()
 
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if no httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
 
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should call AuthUseCase with correct params', () => {
+    const { authUseCaseSpy, sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'iago@email.com',
+        password: '12345678'
+      }
+    }
+    sut.route(httpRequest)
+
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
   })
 })
